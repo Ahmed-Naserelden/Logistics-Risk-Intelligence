57c9054c6c821e10a65111f7402de63315ff4900
Ahmed-Naserelden <an2071497@gmail.com>
2025-07-09 01:24:21 +0300

    fix: update extraction entrypoint script to use python3 directly and redirect stderr to log

--- services/extraction/entrypoint.sh
+++ services/extraction/entrypoint.sh
@@ -6,6 +6,4 @@ sudo service cron start
 sudo service ssh start
 
 # Run seismic events streaming listener
-/usr/local/bin/python3 /extraction/scripts/seismic_events/seismic_listener.py >> /extraction/logs/seismic_listener_wal.log
-
-tail -f /dev/null
+python3 "/extraction/scripts/seismic_events/seismic_listener.py" &>> "/extraction/logs/seismic_listener_wal.log"
