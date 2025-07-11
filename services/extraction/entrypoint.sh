9e3ddb8ed173624a3b1be7591993520e83c23a37
Ahmed-Naserelden <an2071497@gmail.com>
2025-07-12 00:45:52 +0300

    feat: Add itvdelab and cluster_util_db services to docker-compose configuration

--- services/extraction/entrypoint.sh
+++ services/extraction/entrypoint.sh
@@ -5,5 +5,10 @@ sudo service cron start
 
 sudo service ssh start
 
+# Ensure the logs directory exists for the listener logs
+if [ ! -d "/extraction/logs" ]; then
+  mkdir -p /extraction/logs
+fi
+
 # Run seismic events streaming listener
 python3 "/extraction/scripts/seismic_events/seismic_listener.py" &>> "/extraction/logs/seismic_listener_wal.log"
